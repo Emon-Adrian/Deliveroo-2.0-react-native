ff55026dfec03baa8d6974104e8a6ed7637834ff
Emon-Adrian <matovuemon20@gmail.com>
2022-07-15 16:51:44 +0300

    restaurant cards

--- components/FeaturedRow.js
+++ components/FeaturedRow.js
@@ -1,7 +1,8 @@
 /* eslint-disable prettier/prettier */
 import React from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, ScrollView} from 'react-native'
 import { ArrowRightIcon } from 'react-native-heroicons/outline'
+import RestaurantCards from './RestaurantCards'
 
 // eslint-disable-next-line prettier/prettier
 const FeaturedRow = ({id,title,description, featuredCatergory}) => {
@@ -13,6 +14,18 @@ const FeaturedRow = ({id,title,description, featuredCatergory}) => {
         <ArrowRightIcon color='#00CCBB'/>
     </View>
     <Text className='text-gray-500 px-4 text-sm'>{description}</Text>
+
+    <ScrollView
+        horizontal
+        contentContainerStyle={{
+            paddingHorizontal: 15,
+        }}
+        showsHorizontalScrollIndicator={false}
+        className="pt-4"
+    >   
+       {/* Restaurant Cards*/}
+       <RestaurantCards/>
+    </ScrollView>
     </View>
   )
 }
---
+++ components/RestaurantCards.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+
+const RestaurantCards = ({
+    id,
+    imgUrl,
+    title,
+    rating,
+    genre,
+    address,
+    short_description,
+    dishes,
+    long,
+    lat,
+}) => {
+  return (
+    <View>
+        <Text>Emon</Text>
+    </View>
+  )
+}
+
+export default RestaurantCards
